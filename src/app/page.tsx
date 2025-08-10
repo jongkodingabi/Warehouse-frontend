725ff8f1d84af52dcbdbca8add4901f9fbbb2baf
fallennsyahid <umarusyahidmasudi1@gmail.com>
2025-08-10 19:51:52 +0700

    add landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,161 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import { Warehouse, ArrowRight, Store, Rocket } from "lucide-react";
+import Link from "next/link";
+import Footer from "@/components/ui/footer";
 
 export default function Home() {
   return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>src/app/page.tsx</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+    <>
+      <header>
+        <nav className="bg-background flex justify-between items-center px-6 py-4 shadow-custom">
+          <div className="flex items-center">
+            <div className="p-2 bg-primary rounded-sm text-white mr-2">
+              <Warehouse className="w-6 h-6" />
+            </div>
+            <div className="text-text font-semibold text-2xl">
+              Warehouse Management
+            </div>
+          </div>
+          <Link
+            href="/login"
+            className="inline-flex items-center bg-primary px-6 py-2.5 font-medium text-base text-white rounded-sm shadow-md hover:bg-primary/90 transition-colors duration-200 ease-in-out"
+          >
+            <ArrowRight className="mr-2" />
+            Get Started
+          </Link>
+        </nav>
+      </header>
 
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
+      <span className="inline-block absolute left-3/5 top-12 -z-10 bg-gradient-to-br bg-slate-600 to-slate-500 w-16 h-16 rounded-full animate-pulse"></span>
+
+      <span className="inline-block absolute left-9 top-2/6 -z-10 bg-gradient-to-br bg-slate-600 to-slate-500 w-26 h-26 rounded-full animate-pulse"></span>
+
+      <span className="inline-block absolute right-1 top-1/2 -z-10 bg-gradient-to-br bg-slate-600 to-slate-500 w-44 h-44 rounded-full animate-pulse"></span>
+
+      <span className="inline-block absolute left-2/6 -bottom-27 -z-10 bg-gradient-to-br bg-slate-600 to-slate-500 w-12 h-12 rounded-full animate-pulse"></span>
+
+      <div className="flex flex-col items-center justify-center my-16 animate-fade-in">
+        <h1 className="font-bold text-4xl text-secondary mb-4">
+          Warehouse Management
+        </h1>
+        <h2 className="text-primary font-bold text-5xl">Warehouse App</h2>
+      </div>
+
+      <div className="bg-gradient-to-br from-slate-400 to-slate-500 my-16 mx-18 rounded-2xl p-6 shadow-card animate-fade-in">
+        <div className="flex justify-between">
+          <div className="relative top-5 left-7">
             <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
+              src="/3d.png"
+              alt="3D"
+              width={452}
+              height={452}
+              className="relative z-10 mantul"
             />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
+            <span className="absolute bottom-0 left-1/2 -translate-x-1/2 -top-3">
+              <svg
+                width="465"
+                height="463"
+                viewBox="0 0 465 463"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  d="M341.176 42.576C388.6 69.632 431.768 109.456 451.528 159.92C471.288 210.08 467.64 270.272 450.008 326.512C432.376 382.752 400.76 434.736 355.16 453.584C309.56 472.432 250.28 458.144 198.6 447.808C146.616 437.472 102.232 431.392 69.096 408.592C35.96 385.488 14.072 346.272 5.256 305.536C-3.256 265.104 1.912 223.76 8.6 176.336C15.288 129.216 23.496 76.32 54.2 43.792C84.904 11.568 138.104 -0.288011 190.392 0.927989C242.376 1.83999 294.056 15.52 341.176 42.576Z"
+                  fill="#2563EB"
+                />
+              </svg>
+            </span>
+          </div>
+          <div className="flex items-center flex-col ml-40 mr-10">
+            <h1 className="font-bold text-3xl text-center text-text">
+              Solusi Untuk Manajemen Operasi Gudang Anda
+            </h1>
+            {/* 1 */}
+            <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+              <div className="my-5 border-2 border-slate-800 rounded-sm p-5 shadow-custom-2 relative w-80">
+                <span className="absolute -top-3.5 left-1/2 -translate-x-1/2 bg-text text-white px-3 py-1.5 rounded-full font-medium text-xs">
+                  1
+                </span>
+                <div className="flex flex-col items-center mt-1">
+                  <div className="bg-background/25 flex justify-center p-1.5 rounded-full text-text">
+                    <Store />
+                  </div>
+                  <h1 className="mt-1.5 items-center font-medium text-sm text-text">
+                    Manajemen Data Gudang
+                  </h1>
+                  <p className="text-center text-xs text-white mt-2">
+                    Mengelola informasi setiap gudang, termasuk nama, lokasi,
+                    kapasitas, dan detail lainnya.
+                  </p>
+                </div>
+              </div>
+              {/* 2 */}
+              <div className="my-5 border-2 border-slate-800 rounded-sm p-5 shadow-custom-2 relative w-80">
+                <span className="absolute -top-3.5 left-1/2 -translate-x-1/2 bg-text text-white px-3 py-1.5 rounded-full font-medium text-xs">
+                  1
+                </span>
+                <div className="flex flex-col items-center mt-1">
+                  <div className="bg-background/25 flex justify-center p-1.5 rounded-full text-text">
+                    <Store />
+                  </div>
+                  <h1 className="mt-1.5 items-center font-medium text-sm text-text">
+                    Manajemen Data Gudang
+                  </h1>
+                  <p className="text-center text-xs text-white mt-2">
+                    Mengelola informasi setiap gudang, termasuk nama, lokasi,
+                    kapasitas, dan detail lainnya.
+                  </p>
+                </div>
+              </div>
+              {/* 3 */}
+              <div className="mb-5 border-2 border-slate-800 rounded-sm p-5 shadow-custom-2 relative w-80">
+                <span className="absolute -top-3.5 left-1/2 -translate-x-1/2 bg-text text-white px-3 py-1.5 rounded-full font-medium text-xs">
+                  1
+                </span>
+                <div className="flex flex-col items-center mt-1">
+                  <div className="bg-background/25 flex justify-center p-1.5 rounded-full text-text">
+                    <Store />
+                  </div>
+                  <h1 className="mt-1.5 items-center font-medium text-sm text-text">
+                    Manajemen Data Gudang
+                  </h1>
+                  <p className="text-center text-xs text-white mt-2">
+                    Mengelola informasi setiap gudang, termasuk nama, lokasi,
+                    kapasitas, dan detail lainnya.
+                  </p>
+                </div>
+              </div>
+              {/* 4 */}
+              <div className="mb-5 border-2 border-slate-800 rounded-sm p-5 shadow-custom-2 relative w-80">
+                <span className="absolute -top-3.5 left-1/2 -translate-x-1/2 bg-text text-white px-3 py-1.5 rounded-full font-medium text-xs">
+                  1
+                </span>
+                <div className="flex flex-col items-center mt-1">
+                  <div className="bg-background/25 flex justify-center p-1.5 rounded-full text-text">
+                    <Store />
+                  </div>
+                  <h1 className="mt-1.5 items-center font-medium text-sm text-text">
+                    Manajemen Data Gudang
+                  </h1>
+                  <p className="text-center text-xs text-white mt-2">
+                    Mengelola informasi setiap gudang, termasuk nama, lokasi,
+                    kapasitas, dan detail lainnya.
+                  </p>
+                </div>
+              </div>
+            </div>
+            <Link
+              href=""
+              className="inline-flex items-center justify-center bg-primary rounded-lg py-5 w-full font-bold text-2xl text-white shadow-custom-3 hover:bg-primary/80 hover:scale-105 focus:scale-100 active:scale-95 active:bg-primary transition-all duration-300 ease-in-out"
+            >
+              <Rocket className="mr-2" />
+              Coba Sekarang
+            </Link>
+          </div>
         </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+      </div>
+      <Footer />
+    </>
   );
 }
